47970f3f45a03e11bf4e9274f5ae417f2a6026fe
sahil ladhania <sahilladhania@sahils-MacBook-Air.local>
2024-12-11 18:28:23 +0530

    Done some changes.

--- src/pages/common/Login.jsx
+++ src/pages/common/Login.jsx
@@ -1,76 +1,23 @@
 import { Input } from "@/components/ui/input.jsx";
 import {Button} from "@/components/ui/button.jsx";
 import {Link} from "react-router-dom";
-import {useState} from "react";
-import {useDispatch} from "react-redux";
-import {authenticateUser} from "@/services/auth/authService.js";
-import {setUser} from "@/redux/slices/userSlice.js";
-import {login} from "@/redux/slices/authSlice.js";
-import Cookies from "js-cookie";
 
 function Login() {
-    // State Variables
-    const [formData, setFormData] = useState({
-        email: "",
-        password: ""
-    });
-    const dispatch = useDispatch();
-    console.log(dispatch);
-    console.log(useDispatch);
-    console.log(useDispatch());
-
-    // Handler Functions
-    const handleSubmit = async(e) => {
-        e.preventDefault();
-        try{
-            const response = await authenticateUser(formData);
-            const user = response.existingUser;
-            console.log("User logged in successfully : ", user);
-            const token = Cookies.get("jwt"); // Issue in this Line -> Because not getting the Cookies , so cant get the token.
-            if(!token){
-                throw new Error("Authentication token not found");
-            };
-            // 	These dispatch calls send actions (login, setUser) to the Redux store.
-            dispatch(login({token}));
-            dispatch(setUser({
-                firstName: user.firstName,
-                lastName: user.lastName,
-                email: user.email,
-                role: user.role,
-            }));
-            setFormData({
-                email: "",
-                password: ""
-            })
-            console.log("Form submitted : " , formData);
-        }
-        catch(error){
-            console.error("Login failed : ", error.message);
-        }
-    }
-    const handleChange = (e) => {
-        e.preventDefault();
-        const { name , value } = e.target;
-        setFormData({
-            ...formData,
-            [name]: value,
-        })
-    }
     return (
         <>
             <div className="flex items-center justify-center h-screen">
                 <div className="max-w-md w-full bg-white p-8 rounded-lg shadow-md">
                     <h2 className="text-2xl font-bold mb-6 text-center">Welcome Back!</h2>
-                    <form onSubmit={handleSubmit}>
+                    <form>
                         {/* Email or Phone Number Section */}
                         <div className="mb-4">
                             <label className="block text-sm font-medium mb-1">Email or Phone Number</label>
-                            <Input onChange={handleChange} name="email" value={formData.email} placeholder="Enter your email" required/>
+                            <Input name="email" placeholder="Enter your email" required/>
                         </div>
                         {/* Password Section */}
                         <div className="mb-6">
                             <label className="block text-sm font-medium mb-1">Password</label>
-                            <Input onChange={handleChange} type="password" name="password" value={formData.password} placeholder="Enter your password" required/>
+                            <Input type="password" name="password" placeholder="Enter your password" required/>
                         </div>
                         {/* Login Button */}
                         <Button type="submit" className="w-full">Login</Button>
--- src/pages/common/SignUp.jsx
+++ src/pages/common/SignUp.jsx
@@ -1,77 +1,38 @@
 import { Input } from "@/components/ui/input.jsx";
 import { Button } from "@/components/ui/button.jsx";
 import {Link} from "react-router-dom";
-import {useState} from "react";
-import {signUp} from "@/services/auth/authService.js";
 
 function SignUp() {
-    // State Variables
-    const [formData , setFormData] = useState({
-        firstName : "",
-        lastName : "",
-        phoneNumber : "",
-        email : "",
-        password: "",
-    });
-
-    // Handler Functions
-    const handleSubmit = async(e) => {
-        e.preventDefault();
-        try{
-            const response = await signUp(formData);
-            console.log("User signed up successfully:", response);
-        }
-        catch(error){
-            console.log(error.message);
-        }
-        console.log("Form submitted : " , formData);
-        setFormData({
-            firstName : "",
-            lastName : "",
-            phoneNumber : "",
-            email : "",
-            password: "",
-        })
-    }
-    const handleChange = (e) => {
-        e.preventDefault();
-        const { name , value } = e.target;
-        console.log(value);
-        setFormData({
-            ...formData,
-            [name]: value,
-        });
-    }
     return (
         <>
             <div className="flex items-center justify-center h-screen">
                 <div className="max-w-md w-full bg-white p-8 rounded-lg shadow-md">
                     <h2 className="text-2xl font-bold mb-6 text-center">Create an Account</h2>
-                    <form onSubmit={handleSubmit}>
+                    <form>
                         {/* First Name Section */}
                         <div className="mb-4">
                             <label className="block text-sm font-medium mb-1">First Name</label>
-                            <Input onChange={handleChange} type="text" name="firstName" value={formData.firstName} placeholder="Enter your first name" required/>
+                            <Input  type="text" name="firstName"  placeholder="Enter your first name" required/>
                         </div>
                         {/* Last Name Section */}
                         <div className="mb-4">
                             <label className="block text-sm font-medium mb-1">Last Name</label>
-                            <Input onChange={handleChange} name="lastName" value={formData.lastName} placeholder="Enter your last name" required/>
+                            <Input name="lastName"  placeholder="Enter your last name" required/>
                         </div>
                         {/* Email Section */}
                         <div className="mb-4">
                             <label className="block text-sm font-medium mb-1">Email</label>
-                            <Input onChange={handleChange} type="email" name="email" value={formData.email} placeholder="Enter your email" required/>
+                            <Input type="email" name="email"  placeholder="Enter your email" required/>
                         </div>
                         {/* Phone Number Section */}
                         <div className="mb-4">
                             <label className="block text-sm font-medium mb-1">Phone Number</label>
-                            <Input onChange={handleChange} type="tel" name="phoneNumber" value={formData.phoneNumber} placeholder="Enter your phone number" required/>
+                            <Input type="tel" name="phoneNumber"  placeholder="Enter your phone number" required/>
                         </div>
                         {/* Password Section */}
                         <div className="mb-4">
                             <label className="block text-sm font-medium mb-1">Password</label>
-                            <Input onChange={handleChange} type="password" name="password" value={formData.password} placeholder="Enter your password" required/>
+                            <Input type="password" name="password"  placeholder="Enter your password" required/>
                         </div>
                         {/* Signup Button */}
                         <Button type="submit" className="w-full">Sign Up</Button>
